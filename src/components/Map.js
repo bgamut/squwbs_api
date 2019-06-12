2afddb7c0ff443b701feaf30312b7481f6cb0998
Bernard Ahn <bgamut@gmail.com>
2019-06-13 07:20:18 +0900

    removed view from mapbox to check on desktop

--- src/components/Map.js
+++ src/components/Map.js
@@ -40,14 +40,14 @@ function Map (props){
     ]
 
     return(
-        <View>
+        
             <DeckGL 
                 viewState={initialViewState} 
                 controller={true}
                 layers={layers} >
             <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}/>
             </DeckGL>
-        </View>
+        
     )
     // return(
     //     <DeckGL 
