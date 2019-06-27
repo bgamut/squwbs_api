3c7f36880675453931b0e60fbda1343a05f5585c
Bernard Ahn <bgamut@gmail.com>
2019-06-28 04:29:17 +0900

    again

--- src/expressServer/server.js
+++ src/expressServer/server.js
@@ -80,21 +80,26 @@ var app = express();
 // });
 app.set('views', __dirname + '/views');
 //app.set('view engine','jsx')
-// app.engine('jsx',require('express-react-views').createEngine())
-app.set('view engine','ejs')
+app.engine('jsx',require('express-react-views').createEngine())
+//app.set('view engine','ejs')
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('body-parser').json());
 app.use(require('express-session')({ secret: 'keyboard cat',resave:true,saveUninitialized:true }));
 app.use(express.static(path.join(__dirname, '../../build')));
 app.use(express.static(path.join(__dirname, 'html/*/*')));
+app.use(cookieSession({
+  maxAge:100000,
+  keys:["keyboard cat"]
+}))
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
-});
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//  next();
+// });
 var allowedOrigins = [
                       'http://squwbs.herokuapp.com',
                       'https://squwbs.herokuapp.com/'
