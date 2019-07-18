e1fdfc3e2137c0d5b87f07c008193cba68f0de44
Bernard Ahn <bgamut@gmail.com>
2019-07-18 21:54:44 +0900

    split screen now has korean ocr support.

--- src/components/SplitScreen.js
+++ src/components/SplitScreen.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import './css/SplitScreen.css'
-import {View} from 'react-native'
+import {View,Dimensions} from 'react-native'
 import fs from 'fs'
+// import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 // import WebImage from 'react-native-web-image'
+import { TesseractWorker }  from 'tesseract.js';
+const worker = new TesseractWorker();
+// import RNTesseractOcr from 'react-native-tesseract-ocr'
+
+
 class SplitScreen extends Component {
     constructor(props){
         super(props)
         this.state={
-            imgURL:'./icons/256x256.png',
-            baseSixFour:''
+            imgURL:'./temp/jpeg/17.jpeg',
+            baseSixFour:'',
+            orientation:'',
+            dimensions:{height:0,width:0},
+            value:''
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this)
+        this.height=Dimensions.get('window').height
+        this.width=Dimensions.get('window').width
         this.imageHDRef=React.createRef();
+        this.inputRef = React.createRef();
         this.ironImageHd=null
-        this.bg = './icons/16x16.png'
+        this.dimensions={height:0,width:0}
+        this.bg = './temp/jpeg/17.jpeg'
+        this.value=''
         this.imgStyle={
             // backgroundImage:'url('+this.state.baseSixFour+')',
             filter:'invert(88%)',
             zIndex:1,
             
             // backgroundColor:'red',
-            height:100,
-            width:100
+            height:'100%',
+            width:'50%'
         }
     }
     convertBlobToBase64 = blob => new Promise((resolve, reject) => {
@@ -31,13 +47,46 @@ class SplitScreen extends Component {
         };
         reader.readAsDataURL(blob);
     });
+    changeDimensions(dimensions){
+        this.setState({dimensions:dimensions})
+    }
     componentDidMount(){
+        const changeDimensions=()=>{
+            const height = Dimensions.get('window').height
+            const width= Dimensions.get('window').width
+            // const dimensions = {hieght:height,width:width}
+            // this.imageHDRef.current.style.height=height
+            // this.imageHDRef.current.style.width=width
+            // if(this.height!==height){
+                this.height=height
+            // }
+            // if(this.width!==width){
+                this.width=width
+            // }
+            // this.setState({dimensions:dimensions})
+            // console.log(this.state.dimensions)
+            this.forceUpdate();
+        }
+        
+        
+        if ('onorientationchange' in window) {
+            window.addEventListener("orientationchange", function() {
+                changeDimensions()
+                
+                console.log("onorientationchange");
+            }, false);
+        } else if ('onresize' in window) {
+            window.addEventListener("resize", function() {
+                changeDimensions()
+                // console.log("resize");
+            }, false);
+        }
         fetch(this.state.imgURL)
-        // .then(res=>res.arrayBuffer())
+        
         .then(res=>res.blob())
         .then(ab=>this.convertBlobToBase64(ab))
         .then(data=>{
-            console.log(data)
+            // console.log(data)
             this.setState({baseSixFour:data})
         })
         .catch(err=>console.err(err))
@@ -47,9 +96,25 @@ class SplitScreen extends Component {
         //console.log((this.imageHDRef.current.props.style))
         // this.imageHDRef.current.style.width=200
         // this.imageHDRef.current.style.backgroundColor='red'
-        this.imageHDRef.current.style.width='200px'
+        // console.log(this.state.dimensions.height)
+        // console.log(this.state.dimensions.width)
+        // this.imageHDRef.current.style.height=this.state.dimensions.height
+        // this.imageHDRef.current.style.width=this.state.dimensions.width
         console.log(process.env.PUBLIC_URL+this.props.source)
         this.imageHDRef.current.style.backgroundImage="url("+process.env.PUBLIC_URL+this.props.source+")"
+        this.setState({
+            value:"this is how you change up the default text"
+        })
+        
+        changeDimensions()
+        worker.recognize(process.env.PUBLIC_URL+this.props.source,'kor')
+        .progress(progress => {
+            console.log('progress', progress);
+        }).then(result => {
+            console.log('result', result.text);
+            this.setState({value:result.text})
+        });
+
         // console.log((this.imageHDRef.current.style))
         
         // console.log(React.refs)
@@ -74,10 +139,20 @@ class SplitScreen extends Component {
         //     "background-image:url("+this.props.srcLoaded+")"
         //     //"background-image:url("+String(this.state.baseSixFour)+")"
         // )
+        // console.log(RNTesseractOcr)
+        // RNTesseractOcr.recognize(process.env.PUBLIC_URL+this.props.source,'LANG_KOREAN',{whitelist:null,blacklist:null})
+        // .then((result) => {
+        //     this.setState({ ocrResult: result });
+        //     console.log("OCR Result: ", result);
+        //   })
+        //   .catch((err) => {
+        //     console.log("OCR Error: ", err);
+        //   })
+        //   .done();
     }
     componentDidUpdate(){
-        console.log("state changed I think,"+this.state)
-
+        console.log("state changed I think,"+this.state.value)
+        // this.forceUpdate()
     }
     componentWillReceiveProps(newProps){
         // fs.readFile(newProps.source,(err,data)=>{
@@ -100,26 +175,50 @@ class SplitScreen extends Component {
         // process.env.PUBLIC_URL +newProps.source
     }
     handleKeyPress(e){
-        console.log(e.key)
+        // console.log(e.key)
+        console.log(e.target.value)
+        this.setState({value:e.target.value})
+        //console.log(this.inputRef.current.value)
+    }
+    handleChange(e){
+        //this.value=e.target.value
+        this.setState({value:e.target.value})
     }
     render() {
       
         return (
             
             // <WebImage style={this.imgStyle} source={{uri:this.state.baseSixFour}}/>
-            <div
-                style={{height:'100%',width:'100%'}}
+            <View
+                style={{flexDirection:'row'}}
             >
-                <div 
-                 
-                ref={this.imageHDRef}
-                style={{height:100,width:100,backgroundSize: '100% 100%',backgroundRepeat: 'no-repeat'}}
-                // style={{height:100,width:100,backgroundSize: 'contain',backgroundRepeat: 'no-repeat'}}
-                // style={{height:100,width:100,backgroundSize: 'cover',backgroundRepeat: 'no-repeat'}}
-                >
-                    {/* <img src={process.env.PUBLIC_URL + '/icons/256x256.png'} alt="logo"/> */}
-                </div>
-            </div>    
+                    <View >
+                        <div 
+                        
+                            ref={this.imageHDRef}
+                            // style={{height:hp('100%'),width:wp('50%'),backgroundSize: '100% 100%',backgroundRepeat: 'no-repeat'}}
+                            style={{height:this.height,width:this.width/2,backgroundSize: '100% 100%',backgroundRepeat: 'no-repeat'}}
+                        // style={{height:100,width:100,backgroundSize: 'contain',backgroundRepeat: 'no-repeat'}}
+                        // style={{height:100,width:100,backgroundSize: 'cover',backgroundRepeat: 'no-repeat'}}
+                        >
+                            {/* <img src={process.env.PUBLIC_URL + '/icons/256x256.png'} alt="logo"/> */}
+                        </div>
+                    </View>
+                
+                     <View id = 'place-holder2' >
+                        <textarea id='text-input'type='text' spellCheck="false" 
+                            ref={this.inputRef}
+                            value={this.state.value}
+                            //style={{height:hp('100%'),width:wp('50%'),backgroundSize: '100% 100%',backgroundRepeat: 'no-repeat'}} 
+                            style={{height:this.height,width:this.width/2,backgroundSize: '100% 100%',backgroundRepeat: 'no-repeat'}} 
+                            // onKeyPress={this.handleKeyPress}
+                            onChange={this.handleChange}
+                            >
+                        </textarea> 
+                     </View>
+                     
+                
+            </View>    
             
             
             // <Image 
