8dc5675d2c4d904b965811964f93c9a2d05f3b31
Bernard Ahn <bgamut@gmail.com>
2019-08-08 12:24:20 +0900

    redo

--- src/components/UploadWords.js
+++ src/components/UploadWords.js
@@ -7,30 +7,12 @@ import XLSX from 'xlsx'
 import stringifyObject from 'stringify-object'
 const _ = require('lodash')
 
-var firebaseServiceKey = {
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_x509_CERT_URL,
-    "databaseURL":process.env.FIREBASE_DATABASE_URL
-  }
+
 
 
 const withQuery = require('with-query');
 
-const initFirebase=()=>{
-    global.admin = require('firebase-admin')
-    global.admin.initializeApp({
-        credential:global.admin.credential.cert(firebaseServiceKey),
-        databaseURL:firebaseServiceKey.databaseURL
-    })
-}
+
 const addWordToServer = ({word,meaning,example,pronunciation})=>{
     if (example==undefined){
         example=""
@@ -161,10 +143,10 @@ const UploadWords = (props)=> {
                     objList[i]=cloneObject(tempObj)
                 }
                 console.log(objList)
-                initFirebase()
+                //initFirebase()
                 for (var i =0; i<objList.length; i++){
-                    //addWordToServer(objList[i])
-                    addWord(objList)
+                    addWordToServer(objList[i])
+                    //addWord(objList)
                 }
                 //words need to be uploaded to a database now
                
