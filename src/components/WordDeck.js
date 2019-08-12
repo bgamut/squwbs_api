a21884205252e2d8defb5ada164d32c3db82f582
Bernard Ahn <bgamut@gmail.com>
2019-08-12 17:36:19 +0900

    wordDeck changing the method to fetch

--- src/components/WordDeck.js
+++ src/components/WordDeck.js
@@ -3,28 +3,28 @@ import {Animated,PanResponder,Dimensions,View,Text,Image,TouchableHighlight,Touc
 import { Context } from "../context";
 import ReactDOM from 'react-dom'
 import WordCard from './WordCard'
-var admin = require('firebase-admin')
-var firebaseConfig = {
-  apiKey:process.env.FIREBASE_API_KEY
-  ,authDomain:process.env.FIREBASE_AUTH_DOMAIN
-  ,databaseURL:process.env.FIREBASE_DATABASE_URL
-  ,projectId:process.env.FIREBASE_PROJECT_ID
-  ,storageBucket:process.env.FIREBASE_STORAGE_BUCKET
-  ,messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID
-  ,appId:process.env.FIREBASE_APP_ID
-}
-var firebaseServiceKey = {
-  "type": process.env.FIREBASE_TYPE,
-  "project_id": process.env.FIREBASE_PROJECT_ID,
-  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n'),
-  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  "client_id": process.env.FIREBASE_CLIENT_ID,
-  "auth_uri": process.env.FIREBASE_AUTH_URI,
-  "token_uri": process.env.FIREBASE_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.FIREBASE_CLIENT_x509_CERT_URL,
-}
+// var admin = require('firebase-admin')
+// var firebaseConfig = {
+//   apiKey:process.env.FIREBASE_API_KEY
+//   ,authDomain:process.env.FIREBASE_AUTH_DOMAIN
+//   ,databaseURL:process.env.FIREBASE_DATABASE_URL
+//   ,projectId:process.env.FIREBASE_PROJECT_ID
+//   ,storageBucket:process.env.FIREBASE_STORAGE_BUCKET
+//   ,messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID
+//   ,appId:process.env.FIREBASE_APP_ID
+// }
+// var firebaseServiceKey = {
+//   "type": process.env.FIREBASE_TYPE,
+//   "project_id": process.env.FIREBASE_PROJECT_ID,
+//   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+//   "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g,'\n'),
+//   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+//   "client_id": process.env.FIREBASE_CLIENT_ID,
+//   "auth_uri": process.env.FIREBASE_AUTH_URI,
+//   "token_uri": process.env.FIREBASE_TOKEN_URI,
+//   "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+//   "client_x509_cert_url": process.env.FIREBASE_CLIENT_x509_CERT_URL,
+// }
 class WordDeck extends Component {
   constructor(props){
     // admin.initializeApp({
