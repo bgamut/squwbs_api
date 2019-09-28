a227a673c9a9c6e10bc305a1429e5de4abe371f6
B <bgamut@gmail.com>
2019-09-29 08:22:44 +0900

    Updates

---
+++ precache-manifest.b760a64ea080683fee8633f51bfec2f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84d50ad53b6c8e32b5901a6e495342d",
+    "url": "/squwbs_api/index.html"
+  },
+  {
+    "revision": "fe240ce22ba23e40b8ed",
+    "url": "/squwbs_api/static/css/2.50567895.chunk.css"
+  },
+  {
+    "revision": "a3ce95ab6c2b9a5be637",
+    "url": "/squwbs_api/static/css/main.25230c21.chunk.css"
+  },
+  {
+    "revision": "fe240ce22ba23e40b8ed",
+    "url": "/squwbs_api/static/js/2.1b8a4f56.chunk.js"
+  },
+  {
+    "revision": "a3ce95ab6c2b9a5be637",
+    "url": "/squwbs_api/static/js/main.248ec1c2.chunk.js"
+  },
+  {
+    "revision": "ccf547674b019cd11ce8",
+    "url": "/squwbs_api/static/js/runtime~main.d071ff51.js"
+  },
+  {
+    "revision": "9a7fb4165da2ac0d8e8684ec6d9a2d27",
+    "url": "/squwbs_api/static/media/96x96.9a7fb416.png"
+  },
+  {
+    "revision": "8aeca3f08d3249408fb97ebec5f74000",
+    "url": "/squwbs_api/static/media/Alien-Encounters-Solid-Regular.8aeca3f0.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/squwbs_api/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "e94196796fcd701aa3076f765520642a",
+    "url": "/squwbs_api/static/media/close-envelope.e9419679.svg"
+  },
+  {
+    "revision": "4c7ce1b03d039e985ccc196099fde994",
+    "url": "/squwbs_api/static/media/iglogo.4c7ce1b0.svg"
+  },
+  {
+    "revision": "27fa8f26e281456c876d744238443605",
+    "url": "/squwbs_api/static/media/pointer.27fa8f26.svg"
+  }
+]);
