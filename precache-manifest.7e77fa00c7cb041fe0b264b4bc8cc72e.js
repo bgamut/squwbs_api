9f956804302281a97622d624b9ff2b7358f12073
B <bgamut@gmail.com>
2019-09-29 11:47:29 +0900

    Updates

---
+++ precache-manifest.7e77fa00c7cb041fe0b264b4bc8cc72e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e8dff79888d5d890fcdef160d4bdbf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f59be12fc15057a86f19",
+    "url": "./static/css/2.50567895.chunk.css"
+  },
+  {
+    "revision": "5a75e05b210760326b9a",
+    "url": "./static/css/main.ae67cd2b.chunk.css"
+  },
+  {
+    "revision": "f59be12fc15057a86f19",
+    "url": "./static/js/2.4a182f21.chunk.js"
+  },
+  {
+    "revision": "5a75e05b210760326b9a",
+    "url": "./static/js/main.2960a8eb.chunk.js"
+  },
+  {
+    "revision": "c604e4ceb45aa8d7fdee",
+    "url": "./static/js/runtime~main.588e96af.js"
+  },
+  {
+    "revision": "9a7fb4165da2ac0d8e8684ec6d9a2d27",
+    "url": "./static/media/96x96.9a7fb416.png"
+  },
+  {
+    "revision": "8aeca3f08d3249408fb97ebec5f74000",
+    "url": "./static/media/Alien-Encounters-Solid-Regular.8aeca3f0.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "./static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "e94196796fcd701aa3076f765520642a",
+    "url": "./static/media/close-envelope.e9419679.svg"
+  },
+  {
+    "revision": "4c7ce1b03d039e985ccc196099fde994",
+    "url": "./static/media/iglogo.4c7ce1b0.svg"
+  },
+  {
+    "revision": "27fa8f26e281456c876d744238443605",
+    "url": "./static/media/pointer.27fa8f26.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/squwbs_api/precache-manifest.adb77eaae427c4aa7f61474bc72f523d.js"
+  "./precache-manifest.7e77fa00c7cb041fe0b264b4bc8cc72e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/squwbs_api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
