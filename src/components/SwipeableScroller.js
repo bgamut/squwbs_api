3cdea6eb37a09cf2cc48f7e9156295dbf41c8533
B <bgamut@gmail.com>
2019-09-29 11:03:40 +0900

    cname file for ghpages to recognize

--- src/components/SwipeableScroller.js
+++ src/components/SwipeableScroller.js
@@ -776,7 +776,8 @@ const SwipeableScroller = (props) => {
                 <GoogleCard
                   title={'What if the title is unreasonably long. longer than most people tweeting while drunk?'}
                   date={String(date)}
-                  picture={"./icons/256x256.png"}
+                  //picture={"./icons/256x256.png"}
+                  picture={'http://squwbs.herokuapp.com/favicon.ico'}
                   writer={'Sir Francis Dylan the 2nd'}
                   youtubeID={"SkNB5AsgZOc"}
                   post={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
