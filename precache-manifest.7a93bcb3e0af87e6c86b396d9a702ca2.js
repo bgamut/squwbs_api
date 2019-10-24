46d1875feb42c5ce80a06ea1f7de39720efc1cf3
B <bgamut@gmail.com>
2019-10-24 09:51:14 +0900

    Updates

---
+++ precache-manifest.7a93bcb3e0af87e6c86b396d9a702ca2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431e9de09eca222c38ee65cabd55df5c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89dffcf6ac04b0ac4d8d",
+    "url": "./static/css/2.1c7b198e.chunk.css"
+  },
+  {
+    "revision": "e5918a955df349380b2b",
+    "url": "./static/css/main.3b2e3791.chunk.css"
+  },
+  {
+    "revision": "89dffcf6ac04b0ac4d8d",
+    "url": "./static/js/2.8d7c495f.chunk.js"
+  },
+  {
+    "revision": "e5918a955df349380b2b",
+    "url": "./static/js/main.5f08ce17.chunk.js"
+  },
+  {
+    "revision": "c604e4ceb45aa8d7fdee",
+    "url": "./static/js/runtime~main.588e96af.js"
+  },
+  {
+    "revision": "9a7fb4165da2ac0d8e8684ec6d9a2d27",
+    "url": "./static/media/96x96.9a7fb416.png"
+  },
+  {
+    "revision": "8aeca3f08d3249408fb97ebec5f74000",
+    "url": "./static/media/Alien-Encounters-Solid-Regular.8aeca3f0.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "./static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "e94196796fcd701aa3076f765520642a",
+    "url": "./static/media/close-envelope.e9419679.svg"
+  },
+  {
+    "revision": "4c7ce1b03d039e985ccc196099fde994",
+    "url": "./static/media/iglogo.4c7ce1b0.svg"
+  },
+  {
+    "revision": "27fa8f26e281456c876d744238443605",
+    "url": "./static/media/pointer.27fa8f26.svg"
+  }
+]);
